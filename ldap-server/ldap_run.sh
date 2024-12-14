8ec83caa1d20da85ba81056aa55c606d7b0e0a60
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2024-12-15 03:16:57 +0530

    Update ldap_run.sh

--- ldap-server/ldap_run.sh
+++ ldap-server/ldap_run.sh
@@ -21,9 +21,6 @@ done
 # Generate basedomain.ldif from the template
 envsubst < /ldap_config/basedomain.ldif.template > /ldap_config/basedomain.ldif
 envsubst < /ldap_config/chdomain.ldif.template > /ldap_config/chdomain.ldif
-envsubst < /ldap_config/
-
-OPENLDAP_ROOT_PASSWORD=${OPENLDAP_ROOT_PASSWORD:-$ldap_root_passwd}
 
 OPENLDAP_DEBUG_LEVEL=${OPENLDAP_DEBUG_LEVEL:-256}
 
@@ -52,7 +49,7 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
         fi
 
         # Generate hash of password
-        OPENLDAP_ROOT_PASSWORD_HASH=$(slappasswd -s "${OPENLDAP_ROOT_PASSWORD}")
+        OPENLDAP_ROOT_PASSWORD_HASH=$(slappasswd -s "${ldap_root_passwd}")
         echo $OPENLDAP_ROOT_PASSWORD_HASH >> /ldap_root_hash_pw
 
         #Set OpenLDAP admin password.
@@ -67,19 +64,12 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
         # Update configuration with root password and Set your domain name on LDAP DB
         sed -i -e "s OPENLDAP_ROOT_PASSWORD ${OPENLDAP_ROOT_PASSWORD_HASH} g" /ldap_config/chdomain.ldif |
             ldapmodify -Y EXTERNAL -H ldapi:/// -d $OPENLDAP_DEBUG_LEVEL > /dev/null 2>&1
-
-        
-        
+      
+      
         ldapadd -x -D cn=${cn},dc=${base_secondary_dc},dc=${base_primary_dc} -w ${ldap_root_passwd} -f /ldap_config/basedomain.ldif > /dev/null 2>&1
 
 
-        # extract dc name from root DN suffix
-        dc_name=$(echo "${OPENLDAP_ROOT_DN_SUFFIX}" | grep -Po "(?<=^dc\=)[\w\d]+")
-        # create base organization object
-        sed -e "s OPENLDAP_SUFFIX ${OPENLDAP_ROOT_DN_SUFFIX} g" \
-            -e "s FIRST_PART ${dc_name} g" \
-            usr/local/etc/openldap/base.ldif |
-            ldapadd -x -D "$OPENLDAP_ROOT_DN_PREFIX,$OPENLDAP_ROOT_DN_SUFFIX" -w "$OPENLDAP_ROOT_PASSWORD"
+  
 
         # stop the daemon
         pid=$(ps -A | grep slapd | awk '{print $1}')
@@ -101,20 +91,9 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
             exit 1
         fi
     else
-        # We are not root, we need to populate from the default bind-mount source
-        if [ -f /opt/openshift/config/slapd.d/cn\=config/olcDatabase\=\{0\}config.ldif ]
-        then
-            # Use provided default config, get rid of current data
-            rm -rf /var/lib/ldap/*
-            rm -rf /etc/openldap/*
-            # Bring in associated default database files
-            mv -f /opt/openshift/lib/* /var/lib/ldap
-            mv -f /opt/openshift/config/* /etc/openldap
-        else
-            # Something has gone wrong with our image build
-            echo "FAILURE: Default configuration files from /contrib/ are not present in the image at /opt/openshift."
-            exit 1
-        fi
+          # Something has gone wrong with our image build
+          echo "FAILURE: Default configuration files from /contrib/ are not present in the image at /opt/openshift."
+          exit 1
     fi
 
     # Test configuration files, log checksum errors. Errors may be tolerated and repaired by slapd so don't exit
@@ -125,7 +104,7 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
         echo "The file ${err} has a checksum error. Ensure that this file is not edited manually, or re-calculate the checksum."
     done
 
-    rm -rf /opt/openshift/*
+    rm -rf /ldap_config/*.template
 
     touch /etc/openldap/CONFIGURED
 fi
