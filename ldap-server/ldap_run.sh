f5741caba37f063f82d78f604b5fea2e30efe5fc
Satish gupta <95120727+ditissgithub@users.noreply.github.com>
2024-12-17 11:37:27 +0530

    Update ldap_run.sh

--- ldap-server/ldap_run.sh
+++ ldap-server/ldap_run.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
-set -x
+set -xe
+
 # Reduce maximum number of open file descriptors to 1024
 ulimit -n 1024
 
-# Exit immediately if a command exits with a non-zero status
-set -e
-
 # Check if required environment variables are set
-required_vars=(LDAP_ROOT_PASSWD base_primary_dc base_secondary_dc base_subdomain_dc CN OU1 OU2 OU3 OU4 OU5 OU6 OU7 primary_server_uri secondary_server_uri)
+required_vars=(LDAP_ROOT_PASSWD base_primary_dc base_secondary_dc base_subdomain_dc cn ou1 ou2 ou3 ou4 ou5 ou6 ou7 primary_ldap_server_ip secondary_ldap_server_ip)
 
 for var in "${required_vars[@]}"; do
   if [ -z "${!var}" ]; then
@@ -20,10 +18,10 @@ done
 envsubst < /ldap_config/basedomain.ldif.template > /ldap_config/basedomain.ldif
 envsubst < /ldap_config/chdomain.ldif.template > /ldap_config/chdomain.ldif
 envsubst < /ldap_config/ldap.conf.template > /etc/openldap/ldap.conf
+envsubst < /ldap_config/nslcd.conf.template > /etc/nslcd.conf
 
 # Set default OpenLDAP debug level if not provided
 OPENLDAP_DEBUG_LEVEL=${OPENLDAP_DEBUG_LEVEL:-256}
-
 
 # Run initial setup if not already configured
 if [ ! -f /etc/openldap/CONFIGURED ]; then
@@ -34,18 +32,18 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
   fi
 
   # Start slapd in the background
-  slapd -h "ldap:/// ldaps:/// ldapi:///" -d $OPENLDAP_DEBUG_LEVEL &
+  slapd -h "ldap:/// ldaps:/// ldapi:///" -d 256 > /dev/null 2>&1 &
   slapd_pid=$!
 
   # Wait for slapd to start
   for i in {1..30}; do
-    if ldapsearch -Y EXTERNAL -H ldapi:/// -s base -b "cn=config" &>/dev/null; then
+    if ldapsearch -Y EXTERNAL -H ldapi:/// -s base -b "cn=config" > /dev/null 2>&1; then
       break
     fi
     sleep 1
   done
 
-  if ! ps -p $slapd_pid &>/dev/null; then
+  if ! ps -p "$slapd_pid" > /dev/null 2>&1; then
     echo "Error: slapd failed to start." >&2
     exit 1
   fi
@@ -55,27 +53,25 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
   echo "${OPENLDAP_ROOT_PASSWORD_HASH}" > /ldap_root_hash_pw
 
   # Set root password
-  sed -i "s|OPENLDAP_ROOT_PASSWORD|${OPENLDAP_ROOT_PASSWORD_HASH}|g" /ldap_config/chrootpw.ldif | \
-    ldapadd -Y EXTERNAL -H ldapi:/// || { echo "Error: Failed to set root password."; exit 1; }
+  sed -i "s|OPENLDAP_ROOT_PASSWORD|${OPENLDAP_ROOT_PASSWORD_HASH}|g" /ldap_config/chrootpw.ldif
+  ldapadd -Y EXTERNAL -H ldapi:/// -f /ldap_config/chrootpw.ldif|| { echo "Error: Failed to set root password."; exit 1; }
 
   # Add basic schemas
-  for schema in cosine inetorgperson nis; do
-    ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/${schema}.ldif || \
-      { echo "Error: Failed to add schema ${schema}."; exit 1; }
-  done
-
+  ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/cosine.ldif -d $OPENLDAP_DEBUG_LEVEL > /dev/null 2>&1
+  ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/inetorgperson.ldif -d $OPENLDAP_DEBUG_LEVEL > /dev/null 2>&1
+  ldapadd -Y EXTERNAL -H ldapi:/// -f /etc/openldap/schema/nis.ldif -d $OPENLDAP_DEBUG_LEVEL > /dev/null 2>&1
   # Configure the domain
-  sed -i "s|OPENLDAP_ROOT_PASSWORD|${OPENLDAP_ROOT_PASSWORD_HASH}|g" /ldap_config/chdomain.ldif | \
-    ldapmodify -Y EXTERNAL -H ldapi:/// || { echo "Error: Failed to configure domain."; exit 1; }
+  sed -i "s|OPENLDAP_ROOT_PASSWORD|${OPENLDAP_ROOT_PASSWORD_HASH}|g" /ldap_config/chdomain.ldif
+  ldapmodify -Y EXTERNAL -H ldapi:/// -f /ldap_config/chdomain.ldif|| { echo "Error: Failed to configure domain."; exit 1; }
 
   # Add basedomain entries
-  ldapadd -x -D "cn=${CN},dc=${base_secondary_dc},dc=${base_primary_dc}" \
+  ldapadd -x -D "cn=${cn},dc=${base_secondary_dc},dc=${base_primary_dc}" \
     -w "${LDAP_ROOT_PASSWD}" -f /ldap_config/basedomain.ldif || \
     { echo "Error: Failed to add basedomain entries."; exit 1; }
 
   # Stop slapd
-  kill -2 $slapd_pid
-  wait $slapd_pid || { echo "Error: slapd did not stop correctly."; exit 1; }
+  kill -2 "$slapd_pid"
+  wait "$slapd_pid" || { echo "Error: slapd did not stop correctly."; exit 1; }
 
   # Test configuration files
   slaptest || echo "Warning: Configuration test failed. Check the output for details."
@@ -83,7 +79,11 @@ if [ ! -f /etc/openldap/CONFIGURED ]; then
   # Cleanup
   rm -rf /ldap_config/*.template
   touch /etc/openldap/CONFIGURED
+  #start the nslcd service
+  pgrep -x "nslcd" > /dev/null || /usr/sbin/nslcd
 fi
+ssh-keygen -A > /dev/null 2>&1
 
 # Start slapd in the foreground
-exec slapd -h "ldap:/// ldaps:/// ldapi:///" -d $OPENLDAP_DEBUG_LEVEL
+exec slapd -h "ldap:/// ldaps:/// ldapi:///" -d "$OPENLDAP_DEBUG_LEVEL"
+pgrep -x "nslcd" > /dev/null || /usr/sbin/nslcd
